17ba0dd65818faff116915beb4bdaa536c4b0cc2
Serafin <138892893+SneakyMcSnea@users.noreply.github.com>
2024-09-22 15:03:00 +0200

    Add basic functionality to remove one item
    Through the use of the array.filter()-method it is possible to remove one single item from the array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, removeValue) {
+    return array.filter((value) => value != removeValue);
 };
 
 // Do not edit below this line
