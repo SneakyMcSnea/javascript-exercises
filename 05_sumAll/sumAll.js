3fb226659a5f3376baf060d54e8e9f9f1db649e5
Serafin <138892893+SneakyMcSnea@users.noreply.github.com>
2024-09-22 21:52:10 +0200

    Builds sum for first test correctly
    Sum gets built correctly, now its time to test verification condition

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(value1, value2) {
+    if ((value1 < 0 || !typeof value1 === Number || !Number.isInteger(value1) ||
+         value2 < 0 || !typeof value2 === Number || !Number.isInteger(value2)))
+    {
+        return 'ERROR';
+    }
 
+    let startNumber = null;
+    let endNumber = null;
+    let result = null;
+
+    if (value1 > value2){
+        startNumber = value2;
+        endNumber = value1;
+    }
+
+    else if (value2 > value1){
+        startNumber = value1;
+        endNumber = value2;
+    }
+
+    for (let i = startNumber; i <= endNumber; i++){
+        result = result + i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
