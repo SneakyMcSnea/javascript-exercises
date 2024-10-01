5699ce6a179b295c0e1687c254847a613734c099
SneakyMcSnea <138892893+SneakyMcSnea@users.noreply.github.com>
2024-10-01 17:18:52 +0200

    Solved power and factorial successfully

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -20,12 +20,20 @@ const multiply = function(array) {
   }, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;	
 };
 
-const factorial = function() {
-	
+const factorial = function(numFactorial) {
+	if(numFactorial == 0){
+    return 1;
+  }
+
+  let arrayFactorial = Array.from({length: numFactorial}, (_, index) => index + 1);
+  return arrayFactorial.reduce((factorial, current) => {
+    factorial *= current;
+    return factorial;
+  }, 1);
 };
 
 // Do not edit below this line
