4e3a73de3b9c6eccdf300fa70fed75ea21f99884
SneakyMcSnea <138892893+SneakyMcSnea@users.noreply.github.com>
2024-10-10 10:41:38 +0200

    Solved Ex. 09_palindromes successfully

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strArray = Array.from(str);
+    let lettersOnly = strArray.filter((char) => {
+        return (/[A-Za-z0-9]/).test(char)});
+    let lettersOnlyLower = lettersOnly.map((char) => char.toLowerCase())
+    let lettersOnlyReversed = lettersOnlyLower.toReversed();
+    let strLowerLetters = lettersOnlyLower.join("");
+    let strLowerLettersReversed = lettersOnlyReversed.join("");
+    return (strLowerLetters === strLowerLettersReversed) ? true : false;
 };
 
 // Do not edit below this line
