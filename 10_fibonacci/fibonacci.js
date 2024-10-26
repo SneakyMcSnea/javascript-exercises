f3ae48dcce3bfda65dfa6fa45ecb4413b1f6918d
SneakyMcSnea <138892893+SneakyMcSnea@users.noreply.github.com>
2024-10-26 11:43:05 +0200

    Solved Ex. 10 Fibonacci successfully with for loop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibonacciMember) {
++fibonacciMember;
+if (fibonacciMember < 0){
+    return "OOPS";
+}
+if (fibonacciMember == 0){
+    return 0;
+}
+let fibonacciArr = [1, 1];
+let iterations = fibonacciMember - 2;
+for (k = 0; k < iterations; k++){
+    result = fibonacciArr[k] + fibonacciArr[k+1];
+    fibonacciArr.push(result);
+}
+return fibonacciArr[fibonacciMember - 1];
 };
 
 // Do not edit below this line
