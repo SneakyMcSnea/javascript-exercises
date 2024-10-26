b1cdc3c18209beec177ad431a9afb4fcc97dd01b
SneakyMcSnea <138892893+SneakyMcSnea@users.noreply.github.com>
2024-10-26 13:05:23 +0200

    Solved Ex. 12 findTheOldest successfully using reduce()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(peopleArr) {
+    let oldest = peopleArr.reduce((benchmarkObj, currentObj) => {
+        currentYear = new Date().getFullYear();$
+        
+        benchmarkAge = (benchmarkObj.yearOfDeath || currentYear) - benchmarkObj.yearOfBirth;
+        currentAge = (currentObj.yearOfDeath || currentYear) - currentObj.yearOfBirth;
+        if (currentAge > benchmarkAge){
+            return currentObj;
+        }
+        return benchmarkObj;
+        })
+    return oldest;
+    };
 
 // Do not edit below this line
 module.exports = findTheOldest;
